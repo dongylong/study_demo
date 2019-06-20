fcd89131a7ad747d0a0b6b899ac426ad8726cdfa
dongyl <dongyuanlong@100tal.com>
2019-06-20 10:20:17 +0800

    chp2,3

---
+++ crash_course/chp2.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+#2.4.4
+print 3/2.0
+print 3/2
+# 2.4.3
+msg_2_4_3 = "helo, No"+str(10)
+print msg_2_4_3
+# 2.4.2 浮点数
+print ("0.1+0.1:" , 0.1+0.1)
+print ("小数位数可能不精确，0.2+0.1:", 0.2+0.1)
+# 2.4.1 整数
+print("2+3:", 2 + 3)
+#2的3次方
+print("2 ** 3:", 2 ** 3)
+# 2.1
+print("hello python")
+
+#2.2
+message = "hello python msg"
+print(message)
+
+#2.3
+mesage = 'I told you "hello python msg"'
+print(mesage)
+
+#2.3
+mesage = "hello 'python' msg"
+print(mesage)
+
+#2.3.1
+name = " ada lovelAce "
+print("name.title():"+name.title())
+print("name.upper():"+name.upper())
+print("name.lower():"+name.lower())
+#2.3.4
+print("name.rstrip():"+name.rstrip())
+print("name.lstrip():"+name.lstrip())
+print("name.strip():"+name.strip())
+
+msg_2_3_5 = "hello 'python' msg"
+print(msg_2_3_5)
---
+++ crash_course/chp3.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+list = ["tom", "jerry", "cat", "dog"]
+print (list)
+print (list[0])
+print (list[0].title())
+print ("最后一列表元素: " + list[-1])
+
+# 3.2.1 修改元素
+motorcycles = ["honda", "yamaha", "suzuki"]
+print (motorcycles)
+motorcycles.append("ducati")
+print (motorcycles)
